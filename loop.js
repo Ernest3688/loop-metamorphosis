46cb8c8b8b2113a4dcdf1fce3f534f629ba65a4a
Ernest3688 <88985578+Ernest3688@users.noreply.github.com>
2024-03-11 23:20:29 +0100

    first commit

---
+++ loop.js
@@ -0,0 +1,147 @@
+//number 1 for loop
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+//while loop
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+
+  i++;
+}
+
+//Number 2 for loop
+
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+}
+
+//while loop
+
+while (i >= 0) {
+  console.log(i);
+  i--;
+}
+
+//Number 3 for loop
+
+const n = 40;
+for (let i = 1; i <= n; i++) {
+  console.log(i);
+}
+
+//while loop
+
+while (i <= n) {
+  console.log(n);
+  i++;
+}
+
+//Number 4 for loop
+
+for (let i = 0; i <= 7; i++) {
+  console.log("#".repeat(i));
+}
+
+//while loop
+
+while (i <= 7) {
+  console.log("#".repeat(i));
+  i++;
+}
+
+//Number 5 for Loop
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i} x ${i} = ${i * i}`);
+}
+
+//while loop
+while (i <= 10) {
+  console.log(`${i} x ${i} = ${i * i}`);
+  i++;
+}
+
+//Number 6 for loop
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i}   ${i ** 2}   ${i ** 3}`);
+}
+
+//while loop
+
+while (i <= 10) {
+  console.log(`${i}   ${i ** 2}   ${i ** 3}`);
+  i++;
+}
+
+// Number 7 for loop
+
+for (let i = 2; i <= 100; i += 2) {
+  console.log(i);
+}
+
+//while loop
+
+while (i <= 100) {
+  console.log(i);
+  i += 2;
+}
+
+//Number 8 for loop
+
+for (let i = 1; i <= 100; i += 2) {
+  console.log(i);
+}
+
+//while loop
+
+while (i <= 100) {
+  console.log(i);
+  i += 2;
+}
+
+//Number 9 for loop
+
+for (let i = 1; i <= 100; i += 2) {
+  console.log(i);
+}
+
+//while loop
+
+while (i <= 100) {
+  console.log(i);
+  i += 2;
+}
+
+//Number 10 for loop
+
+for (let i = 2; i < num; i++) {
+  console.log(i);
+  i++;
+}
+
+//while loop
+
+const num = 11;
+
+while (i < num) {
+  console.log(i);
+  i++;
+}
+
+//Number 11 for loop
+
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum = sum + i;
+}
+
+//while loop
+
+while (i <= 100) {
+  sum = sum + i;
+  i++;
+}
+
+console.log(sum);
